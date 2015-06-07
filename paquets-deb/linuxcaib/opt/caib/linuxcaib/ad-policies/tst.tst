8133910e2da263b16e3d3046ae087936c705af5f
Sebastia Matas <annonygmouse@yahoo.co.uk>
2015-06-07 23:10:54 +0200

    Esborr carpeta antiga

--- paquets-deb/linuxcaib/opt/caib/linuxcaib/ad-policies/tst.tst
+++
@@ -1,5 +0,0 @@
-
-echo "test"
-echo "test" > /tmp/tst.txt
-
-
--- paquets-deb/linuxcaib/opt/caib/linuxcaib/caib-clean-javaws.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-################################################################################
-# Elimina los acesos directos JavaWebStart
-
-find  .java/ -iname "*.ind" -print0 | xargs -0 rm  2> /dev/null
-
-#Pendent validar funcionament a linux
